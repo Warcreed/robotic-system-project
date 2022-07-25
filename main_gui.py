62bff92efeb16c57e65d1d081ea3eee87fcf3bd3
Danilo Santitto <danilo.santitto@gmail.com>
2022-07-25 18:59:55 +0200

    feat: telemetry implemented

--- main_gui.py
+++ main_gui.py
@@ -65,6 +65,7 @@ class MainWindow(QWidget):
         self.world = World(self)
 
         self.telemetry = Telemetry()
+        self.show_telemetry = True
 
         self._timer_painter = QtCore.QTimer(self)
         self._timer_painter.timeout.connect(self.go)
@@ -97,8 +98,8 @@ class MainWindow(QWidget):
         # add moving to target
         # block = self.world.get_block_slot_at(block_index)
         self.notification = False
-        (target_x, target_y, target_alpha) = self.block_poses[block_index].get_pose()
-        self.arm.set_target_xy_a(target_x, target_y, target_alpha)
+        (target_x, target_y, target_alpha_deg) = self.block_poses[block_index].get_pose()
+        self.arm.set_target_xy_a(target_x, target_y, target_alpha_deg)
 
     def sense_block_presence(self): #da sistemare
         if self._phidias_agent is not None:
@@ -112,7 +113,7 @@ class MainWindow(QWidget):
         self.world.new_block(random.choice(COLOR_NAMES))
 
     def notify_target_got(self):
-        self.notification = True
+        self.notification = True     
         if self._phidias_agent is not None:
             Messaging.send_belief(self._phidias_agent, 'target_got', [], 'robot')
    
@@ -133,24 +134,26 @@ class MainWindow(QWidget):
             Messaging.send_belief(self._phidias_agent, 'block_dropped', [], 'robot')
     
     def go(self):
-        #self.telemetry.gather(self.delta_t, self.arm.element_3_model.w, self.arm.element_3_control.w_target)
-        #if self.t > 8:
-        #    self.telemetry.show()
-        #    sys.exit(0)
+        if self.show_telemetry:
+            self.evaluate_telemetry()
 
-        if self.trajectory.target_got:
+        if self.trajectory.phase == VirtualRobot2D.TARGET:
             if not(self.notification):
                 self.notify_target_got()
 
         self.arm.evaluate_trajectory(self.delta_t)
 
-        #p = self.arm.get_pose()
-        #self.trajectory_data.append(p[-1])
-        #if self.use_trajectory:
-        #    self.target_trajectory_data.append( (self.arm.trajectory_x, self.arm.trajectory_y) )
         self.t += self.delta_t
         self.update() # repaint window
 
+    def evaluate_telemetry(self):
+        base_joint = (self.arm.theta1, self.arm.element_1_model.theta, self.arm.element_1_control.w_target, self.arm.element_1_model.w, self.arm.element_1_control.torque)
+        second_joint = (self.arm.theta2, self.arm.element_2_model.theta, self.arm.element_2_control.w_target, self.arm.element_2_model.w, self.arm.element_2_control.torque)
+        end_effector_joint = (self.arm.theta3, self.arm.element_3_model.theta, self.arm.element_3_control.w_target, self.arm.element_3_model.w, self.arm.element_3_control.torque)
+        self.telemetry.gather(self.t, base_joint, second_joint, end_effector_joint)
+        if self.t > 7:
+           self.telemetry.show(print_base_joint= False, print_second_joint= False, print_end_eff_joint= True)
+           self.show_telemetry = False
 
     def paintEvent(self, event):
         qp = QtGui.QPainter()
@@ -183,193 +186,5 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# #
-# #
-# #
-
-# import sys
-
-# sys.path.insert(0, './lib')
-# sys.path.insert(0, './modules')
-
-# import math
-# import random
-
-# #
-# from PyQt5 import QtGui, QtCore
-# from PyQt5.QtWidgets import QApplication, QWidget
-# # ^^^^^^^^^^^^^^^^^^^^^^^^^^^ use these for Qt5
-# #
-# #
-
-# #from PyQt4 import QtGui, QtCore
-
-# from modules.manipulator.manipulator import *
-# from modules.manipulator.manipulator_painters import *
-# from modules.utils.telemetry import *
-# from modules.world.world import *
-# from modules.phidias.phidias_interface import *
-
-# COLOR_NAMES = [ 'red',
-#                 'green',
-#                 'blue',
-#                 ]
-
-# class MainWindow(QWidget):
-
-#     def __init__(self):
-#         super(MainWindow, self).__init__()
-#         self.initUI()
-
-#     def initUI(self):
-#         self.setGeometry(0, 0, 1000, 600)
-#         self.setWindowTitle('Robotic Arm Project - Danilo Santitto')
-#         self.show()
-
-#         self.delta_t = 1e-3 # 10ms of time-tick
-#         self.t = 0
-
-#         self.trajectory_data = [ ]
-#         self.target_trajectory_data = [ ]
-
-#         self.use_profile = False
-#         self.use_trajectory = True
-#         self.use_angle_interpolation = False
-
-#         self.trajectory = VirtualRobot2D(1.0, 1.5, 1.5)
-
-#         self.arm = ThreeJointsArm(self.trajectory, self.use_profile)
-#         self.painter = ThreeJointsArmPainter(self.arm)
-
-#         target_x = 0.1
-#         target_y = 0.15
-#         target_alpha = -90
-
-#         self.arm.set_target_xy_a(target_x, target_y, target_alpha)
-
-#         self.world = World(self)
-
-#         self.telemetry = Telemetry()
-
-#         self._timer_painter = QtCore.QTimer(self)
-#         self._timer_painter.timeout.connect(self.go)
-#         self._timer_painter.start(int(self.delta_t * 1000))
-#         self.notification = False
-
-#         self._phidias_agent = None
-
-#         self.block_poses = [
-#             Pose(0.115, 0.0675, 0),
-#             Pose(0.215, 0.032, -90),
-#             Pose(0.255, 0.032, -90),
-#             Pose(0.27, 0.11, 0),
-#             Pose(0.265, 0.19, 0),
-#             Pose(0.275, 0.215, 100),
-#             Pose(0.175, 0.295, 0),
-#             Pose(0.155, 0.3, 70),
-#             Pose(0.11, 0.28, 65),
-#             Pose(0.09, 0.27, 130),            
-#         ]
-
-#     def set_phidias_agent(self, _phidias_agent):
-#         self._phidias_agent = _phidias_agent
-
-#     def go_to(self,target_x, target_y, target_alpha):
-#         self.notification = False
-#         self.arm.set_target_xy_a(target_x, target_y, target_alpha)
-    
-#     def go_to_block_slot(self, block_index):
-#         # add moving to target
-#         # block = self.world.get_block_slot_at(block_index)
-#         self.notification = False
-#         (target_x, target_y, target_alpha) = self.block_poses[block_index].get_pose()
-#         self.arm.set_target_xy_a(target_x, target_y, target_alpha)
-
-#     def sense_block_presence(self): #da sistemare
-#         if self._phidias_agent is not None:
-#             d = self.world.sense_block_presence()
-#             params = [d]
-#             Messaging.send_belief(self._phidias_agent, 'is_block_present', params, 'robot')
-
-#     def generate_new_block(self):
-#         if self.world.count_blocks() == 6:
-#             return
-#         self.world.new_block(random.choice(COLOR_NAMES))
-
-#     def notify_target_got(self):
-#         self.notification = True
-#         if self._phidias_agent is not None:
-#             Messaging.send_belief(self._phidias_agent, 'target_got', [], 'robot')
-   
-#     def sense_color(self):
-#         if self._phidias_agent is not None:
-#             d = self.world.sense_color()
-#             params = [] if d is None else [d]
-#             Messaging.send_belief(self._phidias_agent, 'color', params, 'robot')
-    
-#     def collect_block(self):
-#         if self._phidias_agent is not None:
-#             self.world.collect_block()
-#             Messaging.send_belief(self._phidias_agent, 'block_collected', [], 'robot')
-    
-#     def drop_block(self):
-#         if self._phidias_agent is not None:
-#             self.world.drop_block()
-#             Messaging.send_belief(self._phidias_agent, 'block_dropped', [], 'robot')
-    
-#     def go(self):
-#         #self.telemetry.gather(self.delta_t, self.arm.element_3_model.w, self.arm.element_3_control.w_target)
-#         #if self.t > 8:
-#         #    self.telemetry.show()
-#         #    sys.exit(0)
-
-#         if self.trajectory.phase == VirtualRobot2D.TARGET:
-#             if not(self.notification):
-#                 self.notify_target_got()
-
-#         self.arm.evaluate_trajectory(self.delta_t)
-
-#         #p = self.arm.get_pose()
-#         #self.trajectory_data.append(p[-1])
-#         #if self.use_trajectory:
-#         #    self.target_trajectory_data.append( (self.arm.trajectory_x, self.arm.trajectory_y) )
-#         self.t += self.delta_t
-#         self.update() # repaint window
-
-
-#     def paintEvent(self, event):
-#         qp = QtGui.QPainter()
-#         qp.begin(self)
-#         qp.setPen(QtGui.QColor(255,255,255))
-#         qp.setBrush(QtGui.QColor(255,255,255))
-#         qp.drawRect(event.rect())
-
-#         # qp.setPen(QtCore.Qt.black)
-#         # qp.drawLine(50, 500, 900, 500)
-#         # qp.drawLine(50, 500, 50, 50)
-#         # qp.drawLine(50, 50, 900, 50)
-#         # qp.drawLine(900, 50, 900, 500)
-
-#         qp.setPen(QtCore.Qt.black)
-#         self.painter.paint(qp, self.t)
-#         self.world.paint(qp)
-
-#         qp.end()
-
-
-
-# def main():
-
-#     app = QApplication(sys.argv)
-#     ex = MainWindow()
-#     start_message_server_http(ex)
-#     sys.exit(app.exec_())
-
-
-# if __name__ == '__main__':
-#     main()
-
 
 
--- modules/utils/telemetry.py
+++ modules/utils/telemetry.py
@@ -1,40 +1,65 @@
-import pylab
+from lib.data.plot import *
 
 class Telemetry:
 
     def __init__(self):
-        self.reset()
+        self.plotter = DataPlotter()         
 
-    def reset(self):
-        self.times = [ ]
-        self.target_omega_array = [ ]
-        self.target_theta_array = [ ]
-        self.current_omega_array = [ ]
-        self.current_theta_array = [ ]
-        self.t = 0
+    def gather(self, delta_t,  base_joint, second_joint, end_effector_joint):
 
-    def gather(self,delta_t, current_omega, target_omega):
-        self.times.append(self.t)
-        self.target_omega_array.append(target_omega)
-        self.current_omega_array.append(current_omega)
-        self.t = self.t + delta_t
+        self.plotter.add('t', delta_t)
 
-    def show(self):
-        pylab.figure(1)
-        pylab.plot(self.times, self.target_omega_array, 'b-+', label="Omega Target")
-        pylab.plot(self.times, self.current_omega_array, 'r-+', label="Omega Current")
-        pylab.legend()
+        # base_joint
+        self.plotter.add('base_theta_ref', base_joint[0])    # theta from inverse_kinematics
+        self.plotter.add('base_theta', base_joint[1])        # theta from self.arm.element_N.theta
+        self.plotter.add('base_w_target', base_joint[2])     # w target from self.arm.element_N_control.w_target
+        self.plotter.add('base_w', base_joint[3])            # w from position control
+        self.plotter.add('base_torque', base_joint[4])       # torque from speed control
 
-        #pylab.figure(2)
-        #pylab.plot(self.times, self.target_vr_array, 'b-+', label="V Right Target")
-        #pylab.plot(self.times, self.current_vr_array, 'r-+', label="V Right Current")
-        #pylab.legend()
+        # second_joint
+        self.plotter.add('second_theta_ref', second_joint[0])    # theta from inverse_kinematics
+        self.plotter.add('second_theta', second_joint[1])        # theta from self.arm.element_N.theta
+        self.plotter.add('second_w_target', second_joint[2])     # w target from self.arm.element_N_control.w_target
+        self.plotter.add('second_w', second_joint[3])            # w from position control
+        self.plotter.add('second_torque', second_joint[4])       # torque from speed control
 
-        #pylab.figure(3)
-        #pylab.plot(self.times, self.current_w_array, 'r-+', label="Omega Current")
-        #pylab.legend()
+        # end effector
+        self.plotter.add('end_eff_theta_ref', end_effector_joint[0])    # theta from inverse_kinematics
+        self.plotter.add('end_eff_theta', end_effector_joint[1])        # theta from self.arm.element_N.theta
+        self.plotter.add('end_eff_w_target', end_effector_joint[2])     # w target from self.arm.element_N_control.w_target
+        self.plotter.add('end_eff_w', end_effector_joint[3])            # w from position control
+        self.plotter.add('end_eff_torque', end_effector_joint[4])       # torque from speed control
 
-        pylab.show()
+    def show(self, print_base_joint= False, print_second_joint= False, print_end_eff_joint= False):
+        # base_joint
+        if print_base_joint:
+            self.plotter.plot( [ 't', 'Time' ],
+                                [ [ 'base_theta_ref', 'Base Theta Ref'] , [ 'base_theta', 'Base Theta' ] ])
+            self.plotter.plot( [ 't', 'Time' ],
+                                [ [ 'base_w_target', 'Base W Target'], [ 'base_w', 'Base W']  ])
+            self.plotter.plot( [ 't', 'Time' ],
+                                [ [ 'base_torque', 'Base Torque']  ])
+
+        # second_joint
+        if print_second_joint:
+            self.plotter.plot( [ 't', 'Time' ],
+                                [ [ 'second_theta_ref', 'Second Theta Ref'] , [ 'second_theta', 'Second Theta' ] ])
+            self.plotter.plot( [ 't', 'Time' ],
+                                [ [ 'second_w_target', 'Second W Target'], [ 'second_w', 'Second W']  ])
+            self.plotter.plot( [ 't', 'Time' ],
+                                [ [ 'second_torque', 'Second Torque']  ])
+
+        # end effector
+        if print_end_eff_joint:
+            self.plotter.plot( [ 't', 'Time' ],
+                                [ [ 'end_eff_theta_ref', 'End Eff Theta Ref'] , [ 'end_eff_theta', 'End Eff Theta' ] ])
+            self.plotter.plot( [ 't', 'Time' ],
+                                [ [ 'end_eff_w_target', 'End Eff W Target'], [ 'end_eff_w', 'End Eff W']  ])
+            self.plotter.plot( [ 't', 'Time' ],
+                                [ [ 'end_eff_torque', 'End Eff Torque']  ])
+
+        self.plotter.show()
+
 
 
 
